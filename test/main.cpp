25e054d08bc129ec5cab3a96d58170edb178aad6
baixingqiang <bxq2011hust@qq.com>
2016-03-11 14:46:13 +0800

    code reviwe by bxq 2016-3-11-14

--- test/main.cpp
+++ test/main.cpp
@@ -11,9 +11,40 @@
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
+#include<sys/time.h>
 #include<X11/Xlib.h>
 #include<X11/Xutil.h>
 #include<zlib.h>
+
+
+
+int StartAndEndRecordTime(char *filename, int video_time_length)
+{
+	char record_start_time[20]; //starting recording time
+	char record_end_time[20]; //the end of the recording time
+	time_t time_now;
+	struct tm *time_start = NULL;
+	struct tm *time_end = NULL;
+
+	time(&time_now);
+	time_start = localtime(&time_now);
+	//data format: year, month, day, hour, minute, seconds
+	sprintf(record_start_time, "%4d%.2d%.2d%.2d%.2d%.2d",
+			time_start->tm_year + 1900, time_start->tm_mon + 1,
+			time_start->tm_mday, time_start->tm_hour, time_start->tm_min,
+			time_start->tm_sec);
+
+	time_now += video_time_length;
+	time_end = localtime(&time_now);
+	//data format: year, month, day, hour, minute, seconds
+	sprintf(record_end_time, "%d%.2d%.2d%.2d%.2d%.2d", time_end->tm_year + 1900,
+			time_end->tm_mon + 1, time_end->tm_mday, time_end->tm_hour,
+			time_end->tm_min, time_end->tm_sec);
+
+	//filename format: starting recording time - the end of the recording time
+	sprintf(filename, "%s-%s%s", record_start_time, record_end_time, ".hust");
+	return 1;
+}
 
 int WriteBlockToDisk(const void* buffer, size_t compressed_block_size,
 		FILE* stream, unsigned int Frame_number, unsigned int block_x,
@@ -120,6 +151,7 @@ int PutDatablockToXImage(XImage* baseimg, char* block_data,
 	}
 	return 1;
 }
+
 int MyStrcmp(char *s, char *d, int length)//campare the source string and the dest string
 {
 	int i = 0, r = -1;
@@ -142,10 +174,7 @@ int CaptureAndCompare(Display* display, Window desktop, XImage* baseimg,
 	int block_num = 0;	//the length of block_n
 	int screen_width = (int) baseimg->width;
 	int screen_height = (int) baseimg->height;
-/*	if (newimg != NULL)
-		newimg = NULL;
-	newimg = XGetImage(display, desktop, 0, 0, screen_width, screen_height, ~0,
-	ZPixmap);*/
+
 	int tile_width = 64, tile_height = 64, i = 0, j = 0;
 	int n_x = (baseimg->width - 1) / tile_width + 1, tail_x = baseimg->width
 			- tile_width * (n_x - 1);
@@ -235,7 +264,8 @@ int CaptureAndCompare(Display* display, Window desktop, XImage* baseimg,
 	return block_num;  //返回block_n的长度(按block_x,block_y,block_width,block_height)
 }
 
-int CompressAndWrite(const char* filename)
+int CompressAndWrite(const char* filename, int frame_rate,
+		int video_time_length)
 {
 	Window desktop;
 	Display* display;
@@ -279,9 +309,14 @@ int CompressAndWrite(const char* filename)
 	/*	char* new_img_data = (char*) malloc(screen_height * screen_width * 4);
 	 new_img = XCreateImage(display, DefaultVisual(display, 0), 24, ZPixmap, 0,
 	 NULL, screen_width, screen_height, 32, 0);*/
-	for (int m = 0; m < 10; m++)
+	time_t start_time, end_time;
+	time(&start_time);
+	struct timeval start_tv;
+	gettimeofday(&start_tv, NULL);
+	//printf("%s", ctime(&start_time));
+	while (1)
 	{ //控制帧率的块
-		sleep(2);
+	  //sleep(2);
 		new_img = XGetImage(display, desktop, 0, 0, screen_width, screen_height,
 				~0,
 				ZPixmap);
@@ -362,11 +397,28 @@ int CompressAndWrite(const char* filename)
 
 			}
 		}
-		Frame_number++;  //帧号
 
+		Frame_number++;  //帧号
 		free(block_parameter);
 		block_parameter = NULL;
 		XDestroyImage(new_img);
+
+		while (1)
+		{//控制帧率
+			struct timeval end_tv;
+			gettimeofday(&end_tv, NULL);
+			if (((end_tv.tv_sec - start_tv.tv_sec)
+					+ 0.000001 * (end_tv.tv_usec - start_tv.tv_usec))
+					>= Frame_number * 1.0 / frame_rate)
+			{
+				break;
+			}
+		}
+		time(&end_time);
+		if (difftime(end_time, start_time) >= video_time_length)
+		{
+			break;
+		}
 		//控制帧率的块
 	}
 
@@ -376,11 +428,12 @@ int CompressAndWrite(const char* filename)
 	XDestroyImage(base_img);
 	XCloseDisplay(display);
 	fp = NULL;
+	printf("CompressAndWrite perform successfully!");
 	return 1;
 
 }
 
-int UncompressAndDisplay(const char* filename)
+int UncompressAndDisplay(const char* filename, int frame_rate)
 {
 
 	size_t compressed_block_size;
@@ -481,8 +534,9 @@ int UncompressAndDisplay(const char* filename)
 			 }*/
 			XPutImage(display, window, DefaultGC(display, 0), img, 0, 0, 0, 0,
 					screen_width, screen_height);
-			sleep(1);
 			display_flag++;
+
+			usleep(1000000.0 / frame_rate);
 		}
 
 		PutDatablockToXImage(img, img_data, block_x, block_y, block_width,
@@ -507,13 +561,22 @@ int UncompressAndDisplay(const char* filename)
 	return 1;
 }
 
+
 int main()
 {
+	char filename[40];
+	int frame_rate = 25;   //the frame rate
+	int video_time_length = 0.5 * 60; //the totle time of each video, units are seconds
 
-	//CompressAndWrite("./screen");
-	UncompressAndDisplay("./screen");
+	int n = 1;
+	for (int i = 0; i < n; i++)
+	{
+		StartAndEndRecordTime(filename, video_time_length);
+		CompressAndWrite(filename, frame_rate, video_time_length);
+		//UncompressAndDisplay("./20160311140358-20160311140428.hust", frame_rate);
+	}
+
 	printf(" Done.\n");
 
 	return 1;
 }
-
